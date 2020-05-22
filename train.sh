47080bf2cb111bb329002c49a61f1129eb84492f
devin <liliangde_sloth@outlook.com>
2020-05-22 17:01:04 +0800

    max length

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 export BERT_BASE_DIR=../bert/chinese_L-12_H-768_A-12
 export DATA_BASE_DIR=../data/20191029
-python bert_cls.py \
+python bert_siamese.py \
   --data_dir=$DATA_BASE_DIR \
   --task_name=simiq \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --train_batch_size=32 \
-  --max_seq_length=50 \
+  --max_seq_length=30 \
   --learning_rate=1e-5 \
   --num_train_epochs=2.0 \
   --output_dir=./output_dir \
